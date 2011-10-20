299bd2975353d2dd9c33512094944691e78e869d
Alvaro Cuadros-Inostroza <inostroza@mpimp-golm.mpg.de>
2011-10-20 11:18:15 +0000

    New function writeLibText.
    - New function "writeLibText" to save tsLib objects in a tab-delimited
      text file.
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TargetSearch@59451 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/writeLibText.R
@@ -0,0 +1,22 @@
+# function to write a tsLib object to a file.
+# args: - lib: tsLib object
+#       - file: a file
+
+writeLibText <- function(lib, file)
+{
+	stopifnot(validObject(lib))
+	libdata <- data.frame(Name=libName(lib), RI=libRI(lib),
+				Win_1=RIdev(lib)[,1], Win_2=RIdev(lib)[,2],
+				Win_3=RIdev(lib)[,3])
+	libdata$SEL_MASS <- sapply(selMass(lib), function(x) paste(x, collapse=";"))
+	libdata$TOP_MASS <- sapply(topMass(lib), function(x) paste(x, collapse=";"))
+	if(length(spectra(lib)) > 0) {
+		libdata$SPECTRUM <- sapply(spectra(lib), function(x) {
+			paste(apply(x,1, function(y) paste(y[1],y[2],sep=":")), collapse=" ")})
+	}
+	extra <- !colnames(libData(lib)) %in% colnames(libdata)
+	if(any(extra))
+		libdata <- cbind(libdata, libData(lib)[, extra, drop=FALSE])
+    write.table(libdata, file=file, row.names=FALSE, quote=FALSE, sep="\t")
+}
+
