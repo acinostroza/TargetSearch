ce416b4b8b9b503b0b10f92d8bff0988388b8cc5
Alvaro Cuadros-Inostroza <acuadros+github@gmail.com>
2018-05-03 14:39:11 +0200

    Update core functions for NCDF v4
    Add new functionality to NetCDFPeakFinding and peakCDFextraction
    to handle NetCDF v4 files. There are no changes for NetCDF v3.

--- R/NetCDFPeakFinding.R
+++ R/NetCDFPeakFinding.R
@@ -14,8 +14,12 @@ NetCDFPeakFinding <- function(cdfFile, massRange = NULL, Window = 15, IntThresho
 	if(is.na(method))
 		stop("Invalid peak picking method.")
 
-	ncData <- .open.ncdf(cdfFile)
+	# first check that the file is not TS
+	if(.is_ts_ncdf4(cdfFile)) {
+		return(.PeakFinding(cdfFile, Window, IntThreshold, method, baseline, baseline.opts))
+	}
 
+	ncData <- .open.ncdf(cdfFile)
 	if(baseline)
 		ncData <- baseline(ncData, baseline.opts)
 
@@ -27,9 +31,34 @@ NetCDFPeakFinding <- function(cdfFile, massRange = NULL, Window = 15, IntThresho
 			ncData$scanindex, Window, massRange, IntThreshold, PACKAGE="TargetSearch")
 
 	if(method == 2)
-		peaks <- .Call("ppc", ncData, Window, massRange, IntThreshold, PACKAGE="TargetSearch")
+		peaks <- .Call("ppc", ncData, Window, massRange, IntThreshold, NULL, PACKAGE="TargetSearch")
 	colnames(peaks) <- as.character(massRange[1]:massRange[2])
 	return( list(Time = ncData$rt, Peaks = peaks, massRange=massRange) )
+}
+
+# peak finding for version 4
+.PeakFinding <- function(cdfFile, Window, IntThreshold, method, baseline,
+	baseline.opts = NULL)
+{
+	nc      <- nc_open(cdfFile)
+	ncInt   <- ncvar_get(nc, 'intensity')
+	Time    <- ncvar_get(nc, 'retention_time')
+	mzRange <- ncvar_get(nc, 'mass_range')
+	nc_close(nc)
+
+	if(baseline) {
+		ncInt <- do.call(baselineCorrection, append(list(int = t(ncInt)), baseline.opts))
+		ncInt <- t(ncInt)
+	}
+
+	if(method == 1)
+		stop('Error: method not implemented for netCDF-4. use a netCDF-3 file')
+
+	if(method == 2)
+		peaks <- .Call("ppc", NULL, Window, NULL, IntThreshold, ncInt, PACKAGE="TargetSearch")
+
+	colnames(peaks) <- as.character(mzRange[1]:mzRange[2])
+	list(Time = Time, Peaks = peaks, massRange=mzRange)
 }
 
 # vim: set ts=4 sw=4:
--- R/peakCDFextraction.R
+++ R/peakCDFextraction.R
@@ -1,6 +1,15 @@
 `peakCDFextraction` <-
 function(cdfFile, massRange = NULL)
 {
+	if(.is_ts_ncdf4(cdfFile))
+		.peakCDFextraction4(cdfFile, massRange)
+	else
+		.peakCDFextraction3(cdfFile, massRange)
+}
+
+`.peakCDFextraction3` <-
+function(cdfFile, massRange)
+{
 	ncData <- .open.ncdf(cdfFile)
 
 	if(is.null(massRange))
@@ -9,6 +18,18 @@ function(cdfFile, massRange = NULL)
 	peaks <- .Call("ncdfToMatrix", ncData, massRange, PACKAGE="TargetSearch")
 	colnames(peaks) <- as.character(massRange[1]:massRange[2])
 	return( list(Time = ncData$rt, Peaks = peaks, massRange=massRange) )
+}
+
+`.peakCDFextraction4` <-
+function(cdfFile, massRange)
+{
+	nc <- nc_open(cdfFile)
+	on.exit(nc_close(nc))
+	massRange <- ncvar_get(nc, 'mass_range')
+	Time      <- ncvar_get(nc, 'retention_time')
+	peaks     <- ncvar_get(nc, 'intensity')
+	colnames(peaks) <- as.character(massRange[1]:massRange[2])
+	return(list(Time = Time, Peaks = peaks, massRange = massRange))
 }
 
 # vim: set ts=4 sw=4:
