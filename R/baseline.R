6bc1df61c378e48f73eef0d66f0ab35de40eea79
Alvaro Cuadros-Inostroza <acuadros+github@gmail.com>
2019-07-29 10:47:50 +0200

    Restructuring of baseline corrrection functions
    New baseline correction algorthim based on quantiles around a time
    window. The algorithm is applied on every ion trace. The function
    accepts a ncdf list or a matrix as input.
    
    Refactor the classic baselineCorrection method such it takes a ncdf
    list or a matrix. Note the input matrix is transposed respect to
    old TargetSearch versions.
    
    Simplify `baseline` function. Takes either a cdfFile or a ncdf list.
    Moreover, a new parameter bsline_method can be passed to chose between
    the aforementioned methods.

--- R/baseline.R
+++ R/baseline.R
@@ -1,37 +1,25 @@
 # a wrapper for baseline Correction Function
+#
+baseline <- function(ncdf, bsline_method = c('classic', 'quantiles', 'none'), ...)
+{
+    # assume `ncdf` is a file name
+    ncdf <- .peakExtractWrap(ncdf)
 
-baseline <- function(ncData, baseline.opts = NULL) {
+    method <- match.arg(bsline_method)
 
-    foo <- function(x, y, si, pc) y[si[x] + 1:pc[x]]
-
-    if(all(ncData$point_count == ncData$point_count[1]))
-    {
-        int <- sapply(seq_along(ncData$scanindex), foo, ncData$intensity, ncData$scanindex, ncData$point_count)
-        if(!is.matrix(int))
-            stop("Unexpected Error: Object 'int' is not a matrix")
-    }
-    else
-    {
-        massRange <- range(ncData$mz)
-        int <- t(.Call(c_ncdf_to_matrix, ncData, massRange, PACKAGE="TargetSearch"))
-    }
-
-    int <- do.call(baselineCorrection, append(list(int = int), baseline.opts))
-    ## update ncData components...
-    int.id  <- lapply(1:length(ncData$scanindex), function(x) which( int[,x] > 0 ))
-    ncData$intensity <- as.vector( int[int > 0] )
-    ncData$mz <- range(ncData$mz)
-    ncData$mz <- ncData$mz[1]:ncData$mz[2]
-    ncData$mz <- unlist(sapply(int.id, function(x) ncData$mz[x]))
-    ncData$point_count  <- sapply(int.id, length)
-    ncData$scanindex  <- c(0, cumsum(ncData$point_count))[1:length(ncData$point_count)]
-    ncData
+    if(method == 'none')
+        return(ncdf)
+    if(method == 'classic')
+        return(baselineCorrection(ncdf, ...))
+    if(method == 'quantiles')
+        return(baselineCorrectionQuant(ncdf, ...))
+    return(NULL)
 }
 
 # baseline correction
 # args:
-#   int : An intensity matrix where columns represent scan times and rows
-#         differents mass values.
+#   int : a named two component list: `Time` and `Peaks` returned by a call to
+#         peakCDFextraction.
 #   threshold: a threshold for the baseline. A value of one returns a baseline
 #              just above the noise, 0.5 in the middle of the noise and 0 below
 #              the noise.
@@ -41,47 +29,63 @@ baseline <- function(ncData, baseline.opts = NULL) {
 #         considered to be noise.
 #   signalwindow:
 
-baselineCorrection <- function(int, threshold = 0.5, alpha = 0.95, bfraction = 0.2,
+baselineCorrection <- function(peaks, threshold = 0.5, alpha = 0.95, bfraction = 0.2,
     segments = 100, signalWindow = 10, method = "linear") {
 
-        n <- ncol(int)
-        bfrac <- round(bfraction * segments)
-        met <- pmatch(method, c("linear", "spline"))[1]
-        if(is.na(met))
-            stop("Invalid method '", method, "' for baseline correction")
+    # core function for baseline correction. It expects a single vector
+    # of intensity values
+    base_fun <- function(x, threshold, alpha, bfrac, segments, signalWindow, method)
+    {
+        # apply high pass filter
+        xf <- hpf(x, alpha)
+        n  <- length(x)
+        # divide xf in segments
+        np <- ceiling( n / segments )
+        segment.idx  <- rep(1:segments, each = np)[1:n]
+        # compute standard deviation of every segment
+        segment.sd <- sapply( split(xf, segment.idx), sd)
+        # get standard deviation of the 'bfraction' segments with
+        # the lowest standard deviation. (An estimation of
+        # the sd of the noise)
+        o  <- order(segment.sd)[1:bfrac]
+        stdn <- sd(xf[segment.idx %in% o])
+        # points with absolute values higher than 2*stdn are considered as signal
+        # and the center of a signal window of width signalwindow
+        tmp  <- which(abs( xf ) > 2*stdn)
+        # windowing step: apply signalwindow to the signal points obtained before
+        sm   <- windowingStep(tmp, n, signalWindow)
+        sm[1] <- sm[n] <- FALSE
+        if(method == 'spline') {
+            # Fit a cubic smoothing spline of the baseline using the points
+            # considerated noise.
+            sp <- smooth.spline((1:n)[!sm], x[!sm])
+            spx <- predict(sp, 1:n)
+        } else {
+            spx <- approx( (1:n)[!sm], x[!sm], 1:n )
+        }
+        # returns the smoothed points plus offset
+        spx$y + 2 * (threshold - 0.5) * 2 * stdn
+    }
 
-        baseline <- apply(int, 1, function(x) {
-                # apply high pass filter
-                xf <- hpf(x, alpha)
-                # divide xf in segments
-                np <- ceiling( n / segments )
-                segment.idx  <- rep(1:segments, each = np)[1:n]
-                # compute standard deviation of every segment
-                segment.sd <- sapply( split(xf, segment.idx), sd)
-                # get standard deviation of the 'bfraction' segments with
-                # the lowest standard deviation. (An estimation of
-                # the sd of the noise)
-                o  <- order(segment.sd)[1:bfrac]
-                stdn <- sd(xf[segment.idx %in% o])
-                # points with absolute values higher than 2*stdn are considered as signal
-                # and the center of a signal window of width signalwindow
-                tmp  <- which(abs( xf ) > 2*stdn)
-                # windowing step: apply signalwindow to the signal points obtained before
-                sm   <- windowingStep(tmp, n, signalWindow)
-                sm[1] <- sm[n] <- FALSE
-                if(met == 2) {
-                # Fit a cubic smoothing spline of the baseline using the points
-                # considerated noise.
-                    sp <- smooth.spline((1:n)[!sm], x[!sm])
-                    spx <- predict(sp, 1:n)
-                } else if (met == 1)
-                    spx <- approx( (1:n)[!sm], x[!sm], 1:n )
-                # returns the smoothed points plus offset
-                spx$y + 2 * (threshold - 0.5) * 2 * stdn
-        })
-        int <- int - t(baseline)
-        int[int < 0] <- 0
-        int
+    int <- NULL
+    method <- match.arg(method)
+    bfrac <- round(bfraction * segments)
+
+    if(is.list(peaks))
+        int <- peaks$Peaks
+    if(is.matrix(peaks))
+        int <- peaks
+    if(!is.matrix(int))
+        stop("Invalid input object. Expecting a netCDF object or matrix")
+
+    baseline <- apply(int, 2, base_fun, threshold, alpha, bfrac, segments, signalWindow, method)
+    int <- int - baseline
+    int[ int < 0 ] <- 0
+    if(is.matrix(peaks))
+        return(int)
+    peaks$Peaks <- int
+    peaks$baselineCorrected <- TRUE
+    peaks
 }
 
 # high pass filter
@@ -98,6 +102,63 @@ windowingStep <- function(x, n, wm) {
         out <- .C(c_windowing, s = integer(n), x = as.integer(x), wm = as.integer(wm),
                 n = as.integer(n), nidx = as.integer(x.len),  PACKAGE="TargetSearch" )
         as.logical(out$s)
+}
+
+#' baseline correction of MS data. (quantiles)
+#'
+#' Use the quantiles method to estimate the baseline
+#'
+#' @param peaks The netCDF object or a matrix
+#' @param time the retention time in seconds. used if `peaks` is a matrix.
+#' @param smooth integer. Smooth each signal by this number of points.
+#'   Smoothing is disabled if this value is less or equal than 1.
+#' @param qntl numeric scalar. The quantile for baseline estimation.
+#'   The value must be in [0, 1].
+#' @param width numeric scalar. The size of the window centered around
+#'   a scan for baseline estimation.
+#'   The size depends on the parameter `unit` below.
+#' @param unit The width unit, which can be in seconds or points.
+#' @param steps integer scalar greater than zero. To speed up computation, the baseline
+#'   algorithm does not compute the estimate in each single scan, but in intervals of `steps`
+#'   steps. The intermediate points are estimated by simple linear regression.
+#' @return a netCDF object list or a baseline corrected matrix
+baselineCorrectionQuant <- function(peaks, time, smooth=0, qntl=0.50, width=30, unit=c("seconds", "points"), steps=10)
+{
+    bslinefun <- function(x, ...) .Call(TargetSearch:::c_baseline, x, ...)
+    stopifnot(all(!is.na(c(smooth, qntl, width, steps))))
+    int <- NULL
+    unit <- match.arg(unit)
+
+    if(is.list(peaks)) {
+        int <- peaks$Peaks
+        t <- switch(unit, seconds=peaks$Time, points=seq(peaks$Time))
+    }
+    if(is.matrix(peaks)) {
+        int <- peaks
+        t <- if(unit == 'points') seq(nrow(int)) else time
+    }
+    if(!is.matrix(int))
+        stop("Invalid input object. Expecting a netCDF object or matrix")
+
+    # Compute the baseline
+    bsline <- apply(int, 2, bslinefun, t, qntl, width, steps)
+
+    int <- int - bsline
+    int[ int < 0 ] <- 0
+    smooth <- round(smooth)
+    if(smooth > 1)
+        int <- apply(int, 2, filter, rep(1, smooth) / smooth)
+
+    if(is.matrix(peaks))
+        return(int)
+
+    keep <- rowSums(is.na(int)) == 0
+    peaks$Peaks <- int[keep, ]
+    peaks$Time  <- peaks$Time[ keep ]
+    if(!is.null(peaks$Index))
+        peaks$Index <- peaks$Index[ keep ]
+    peaks$baselineCorrected <- TRUE
+    peaks
 }
 
 # vim: set ts=4 sw=4 et:
