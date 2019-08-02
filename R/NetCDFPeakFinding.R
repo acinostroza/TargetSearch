7238ca941dd62034cd6832f68f54d3b38e20f5e2
Alvaro Cuadros-Inostroza <acuadros+github@gmail.com>
2019-08-02 17:52:14 +0200

    Refactor and clean-up of peak picking R functions
    Significant change: The parameter baseline.opts is deprecated.
    Arguments for baseline correction are passed with `...`

--- R/NetCDFPeakFinding.R
+++ R/NetCDFPeakFinding.R
@@ -1,20 +1,16 @@
 # function to find peaks in chromatograms
 # arguments:
-#    - cdfFile : The file
+#    - cdfFile : The file (version 3 or 4)
 #    - Window: Window to be used by method
 #    - IntThreshold: Intensity threshold
 #    - method: peak picking method. PPC / smoothing
 #    - massRange: m/z range (deprecated, it has no effect)
 
 NetCDFPeakFinding <- function(cdfFile, massRange = NULL, Window = 15, IntThreshold = 10,
-	pp.method = "ppc", baseline = FALSE, baseline.opts = NULL) {
+	pp.method = "ppc", baseline = FALSE, ...) {
 
-	method <- pmatch(pp.method,c("smoothing", "ppc", "gaussian"))
-
-	if(is.na(method))
-		stop("Invalid peak picking method.")
-
-	method <- c("s", "p", "g")[method]
+	method <- match.arg(pp.method,c("smoothing", "ppc", "gaussian"))
+	method <- substring(method, 1, 1)
 
 	# methods smoothing and gaussian use W points, while ppc uses 2*W + 1
 	# make the number of points homogeneous,
@@ -22,42 +18,16 @@ NetCDFPeakFinding <- function(cdfFile, massRange = NULL, Window = 15, IntThresho
 
 	refine <- min(c(round(Window / 3), 4))
 
-	# first check that the file is not TS
-	if(.is_ts_ncdf4(cdfFile)) {
-		return(.PeakFinding(cdfFile, method, Window, IntThreshold, refine, baseline, baseline.opts))
-	}
-
-	ncData <- .open.ncdf(cdfFile)
-	massRange <- range(ncData$mz)
+	ncData <- .peakExtractWrap(cdfFile)
 
 	if(baseline)
-		ncData <- baseline(ncData, baseline.opts)
+		ncData <- baseline(ncData, ...)
 
-	peaks <- .Call(c_peak_detection_main, method, ncData, Window, refine, IntThreshold, NULL)
+	peaks <- .Call(c_peak_detection_main, method, NULL, Window, refine, IntThreshold, ncData$Peaks)
 
+	massRange <- ncData$massRange
 	colnames(peaks) <- as.character(massRange[1]:massRange[2])
-	return( list(Time = ncData$rt, Peaks = peaks, massRange=massRange) )
-}
-
-# peak finding for version 4
-.PeakFinding <- function(cdfFile, method, Window, IntThreshold, refine, baseline,
-	baseline.opts = NULL)
-{
-	nc      <- nc_open(cdfFile)
-	ncInt   <- ncvar_get(nc, 'intensity')
-	Time    <- ncvar_get(nc, 'retention_time')
-	mzRange <- ncvar_get(nc, 'mass_range')
-	nc_close(nc)
-
-	if(baseline) {
-		ncInt <- do.call(baselineCorrection, append(list(int = t(ncInt)), baseline.opts))
-		ncInt <- t(ncInt)
-	}
-
-	peaks <- .Call(c_peak_detection_main, method, NULL, Window, refine, IntThreshold, ncInt)
-
-	colnames(peaks) <- as.character(mzRange[1]:mzRange[2])
-	list(Time = Time, Peaks = peaks, massRange=mzRange)
+	return( list(Time = ncData$Time, Peaks = peaks, massRange=massRange) )
 }
 
 # vim: set ts=4 sw=4:
