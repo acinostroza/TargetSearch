dd43d0b3e70b20e072cf859b4c087d296c23c08d
Alvaro Cuadros-Inostroza <acuadros+github@gmail.com>
2019-08-03 09:08:49 +0200

    Add support for NCDF4 files in RI Correction
    This commit add many changes to the RIcorrect function. Notably a new
    parameter was added while another was removed.
    
     - Automatic convertion from NCDF3 to NCDF4. This is controlled by
       new parameter `writeCDF4path`. Set to FALSE by default so it is
       compatible with older examples and vignettes.
    
     - Automatic update of retention time indices on NCDF4 files.
    
     - Argument `baseline.opts` is replaced by `...`. Extra arguments are
       passed to `baseline`. This may break compatibility with older scripts.

--- R/RIcorrect.R
+++ R/RIcorrect.R
@@ -1,7 +1,6 @@
 RIcorrect <- function(samples, rimLimits = NULL, massRange=NULL, Window, IntThreshold,
-	pp.method = "ppc", showProgressBar = FALSE,
-	baseline = FALSE, baseline.opts = NULL ) {
-
+	pp.method="ppc", showProgressBar=FALSE, baseline=FALSE, writeCDF4path=TRUE, ...)
+{
 	manyFiles <- CDFfiles(samples)
 	outFile   <- RIfiles(samples)
 	Names     <- sampleNames(samples)
@@ -20,12 +19,36 @@ RIcorrect <- function(samples, rimLimits = NULL, massRange=NULL, Window, IntThre
 		stop("These files don't exist: ", paste(manyFiles[!file.exists(manyFiles)], collapse = " "))
 	}
 
+	# get cdf4Files depending on the value of writeCDF4path.
+	cdf4Files <- .mk_cdf_files(manyFiles, writeCDF4path)
+	writeCDF4 <- !is.null(cdf4Files)
+
 	if(showProgressBar)
 		pb <- ProgressBar(title="Extracting peaks...", label="File in processing...")
 
 	for(i in 1:length(manyFiles)) {
-		Peaks  <- NetCDFPeakFinding(manyFiles[i], massRange, Window, IntThreshold, pp.method = pp.method,
-				baseline = baseline, baseline.opts = baseline.opts)
+		ncdfInfo <- .ncdf_info(manyFiles[i])
+		ncdf <- peakCDFextraction(manyFiles[i])
+
+		# baseline correction
+		if(baseline) {
+			if(ncdfInfo$baseline_corrected == 1)
+				warning("File '", manyFiles[i], "' already baseline corrected. Skipping")
+			else
+				ncdf <- baseline(ncdf, ...)
+		}
+
+		# writing NCDF-4
+		if(writeCDF4) {
+			if(ncdfInfo$creator == 'TargetSearch') {
+				if(ncdfInfo$baseline_corrected == 0 & baseline)
+					ncdf4_write(cdf4Files[i], ncdf)
+			} else {
+				ncdf4_write(cdf4Files[i], ncdf)
+			}
+		}
+
+		Peaks  <- NetCDFPeakFinding(ncdf, massRange, Window, IntThreshold, pp.method = pp.method)
 
 		# mass range has no effect, so we ignore
 		massRange <- Peaks$massRange
@@ -44,9 +67,15 @@ RIcorrect <- function(samples, rimLimits = NULL, massRange=NULL, Window, IntThre
 			RIcheck[,i] <- fameTimes
 			riInde <- rt2ri(Peaks$Time, fameTimes, standard)
 			writeRIFile(outFile[i], Peaks, riInde, massRange, ftype)
+
+			# update RI on CDF file
+			if(writeCDF4)
+				ncdf4_update_ri(cdf4Files[i], fameTimes, standard)
+
 		} else {
 			writeRIFile(outFile[i], Peaks, Peaks$Time, massRange, ftype)
 		}
+
 		if(showProgressBar)
 			setProgressBar(pb, value=i/length(manyFiles),
 				title=paste("Extracting peaks (", round(100*i/length(manyFiles)), "%)"),
@@ -62,6 +91,36 @@ RIcorrect <- function(samples, rimLimits = NULL, massRange=NULL, Window, IntThre
 	} else {
 		return(NULL)
 	}
+}
+
+#' make NCDF4 from CDF files.
+#'
+#' Generates a list of NCDF4 files from a list of CDF files (formats 3 or 4).
+#' The output is controlled by the parameter path, for which there are several
+#' options:
+#'  - If path is FALSE or NULL or NA, then return NULL
+#'  - If path is TRUE, return a list of NCDF4 files. If the files are already
+#'    NCDF4 files, then output them.
+#-  - If path is a string, then update the files' path to this value. Recycle
+#'    as necessary.
+#' @param files A list of NCDF3 or NCDF4 files
+#' @param path A new path or FALSE.
+#' @exts A list of valid extension which will be used to remove from the input files.
+#' get a list nc4 files. If the files are already nc4 files,
+.mk_cdf_files <- function(files, path, exts=c('nc4','cdf'))
+{
+	if(is_nullOrNA(path))
+		return(NULL)
+	if(is.logical(path)) {
+		if(all(path))
+			return(sprintf("%s.nc4", .trim_file_ext(files, exts)))
+		return(NULL)
+	}
+	if(is.character(path)) {
+		files <- .setpath(files, path)
+		return(sprintf("%s.nc4", .trim_file_ext(files, exts)))
+	}
+	stop("Invalid parameter `path`. logical or character argument expected")
 }
 
 # vim: set ts=4 sw=4:
