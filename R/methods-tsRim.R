4727cb89fb4ab4431fc31443b0db4837d63dc442
Alvaro Cuadros-Inostroza <acuadros+github@gmail.com>
2020-04-16 14:20:28 +0200

    Add selecting method for tsRim-class

--- R/methods-tsRim.R
+++ R/methods-tsRim.R
@@ -6,7 +6,7 @@ setGeneric("rimMass<-", function(obj, value) standardGeneric("rimMass<-"))
 setReplaceMethod("rimMass", "tsRim", function(obj, value) { obj@mass <- value
  validObject(obj)
  obj })
- 
+
 setGeneric("rimLimits", function(obj) standardGeneric("rimLimits"))
 setMethod("rimLimits", "tsRim", function(obj) obj@limits)
 setGeneric("rimLimits<-", function(obj, value) standardGeneric("rimLimits<-"))
@@ -24,7 +24,7 @@ setReplaceMethod("rimStandard", "tsRim", function(obj, value) { obj@standard <- 
 setValidity("tsRim", function(object) {
 	if(ncol(object@limits) != 2)
 		paste("Number of columns of limits it is not equal to 2")
-	else if(nrow(object@limits) != length(object@standard)) 
+	else if(nrow(object@limits) != length(object@standard))
 		paste("Unequal number of standards and limits: ", length(object@standard),
 			", ", nrow(object@limits), sep = "")
 	else if(length(object@mass) != 1 & length(object@mass) != length(object@standard))
@@ -33,4 +33,29 @@ setValidity("tsRim", function(object) {
 	else TRUE
 })
 
+setMethod("[", "tsRim",
+    function(x, i, j, ..., drop=TRUE)
+    {
+        if(missing(i) & missing(j)) {
+            validObject(x)
+            return(x)
+        }
+        if(!missing(j))
+            stop("incorrect number of dimensions")
 
+        if(is.character(i)) {
+            if(is.null(rownames(x@limits)))
+                stop("subscript out of bounds")
+            i <- match(i, rownames(x@limits))
+        }
+        assert_that(noNA(i), msg="subscript out of bounds")
+
+        x@limits <- x@limits[i,,drop=FALSE]
+        x@standard <- x@standard[i]
+        x@mass <- if(length(x@mass) == 1) x@mass else x@mass[i]
+        validObject(x)
+        return(x)
+    }
+)
+
+# vim: set ts=4 sw=4 et:
