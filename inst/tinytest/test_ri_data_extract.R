fa459267ee81ce95e989ab422ac4ade9a3a07b47
Alvaro Cuadros-Inostroza <acuadros+github@gmail.com>
2020-09-14 16:25:30 +0200

    Add function ri_data_extract
    Function to extract peaks from RI files similar to ncdf4_data_extract

---
+++ inst/tinytest/test_ri_data_extract.R
@@ -0,0 +1,19 @@
+require('TargetSearchData')
+
+path <- file.path(find.package("TargetSearchData"), "gc-ms-data")
+id <- c(4,6,7,8,9,11,12,15)
+
+files <- file.path(path, sprintf('RI_7235eg%02d.txt', id))
+
+expect_equal(file.exists(files), rep(TRUE, 8))
+
+a <- lapply(files, ri_data_extract,  204, c(200, 300), useRT=TRUE)
+b <- c(26L, 18L, 22L, 28L, 23L, 33L, 27L, 20L)
+expect_equal(sapply(a, nrow), b)
+
+a <- lapply(files, ri_data_extract, 116, c(200, 220) * 1000)
+b <- c(1L, 1L, 0L, 1L, 1L, 1L, 1L, 1L)
+expect_equal(sapply(a, nrow), b)
+
+a <- lapply(files, ri_data_extract, 1234, c(200, 220) * 1000)
+expect_equal(sapply(a, nrow), integer(8))
