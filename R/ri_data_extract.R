cdcd1b1d51c3b62946a6e3f294afa455090c5fcb
Alvaro Cuadros-Inostroza <acuadros+github@gmail.com>
2021-04-13 17:43:05 +0200

    ri_data_extract: time range can be a matrix
    Formerly, the searched time interval (parameter `timeRange`) was limited
    to a single ranged for all the searched masses. This commit allows to search
    for masses in different time ranges adding more flexibility.
    
    The parameter `timeRange` can be either a matrix or a vector of even length;
    it will be coerced to matrix internally.

--- R/ri_data_extract.R
+++ R/ri_data_extract.R
@@ -5,19 +5,27 @@
 #'
 #' @param RIfile A path to a RI file format (binary or text).
 #' @param massValues A numeric vector representing m/z values.
-#' @param timeRange A numeric vector of length 2 (lower and upper time limits)
+#' @param timeRange A numeric vector of even length describing the lower and upper time limits
 #' @param useRT Logical. If `TRUE`, the time range is in seconds.
 #' @param ... extra parameters passed to internal functions
 #' @return A four column matrix with columm names RI, RT, Intensity, mz.
 #'
 `ri_data_extract` <- function(RIfile, massValues, timeRange, useRT=FALSE, ...)
 {
-    assert_that(is.numeric(timeRange), length(timeRange) == 2)
+    assert_that(is.string(RIfile))
     assert_that(is.numeric(massValues))
-    ref <- cbind(minRI=timeRange[1], mz=massValues, maxRI=timeRange[2])
-    result <- getAllPeaks(RIfile, ref, useRT, ...)
-    result <- result[, c('RI', 'RT', 'Int', 'mz'), drop=FALSE]
-    colnames(result)[3] <- 'Intensity'
+    assert_that(is.numeric(timeRange), length(timeRange) %% 2 == 0)
+    assert_that((length(massValues) %% (length(timeRange) / 2)) == 0)
+    assert_that(is.flag(useRT))
+    if(!is.matrix(timeRange)) timeRange <- matrix(timeRange, ncol=2)
+    assert_that(ncol(timeRange) == 2)
+
+    ref <- cbind(mz=massValues, minRI=timeRange[,1], maxRI=timeRange[,2])
+    opt <- as.integer(get.file.format.opt(RIfile, ...))
+    result <- .Call(c_find_peaks, RIfile, as.integer(ref[,'mz']), NULL, ref[,'minRI'],
+                                  ref[,'maxRI'], opt, useRT, 1L)
+    result <- cbind(RI=result[[2]], RT=result[[3]], Intensity=result[[1]],
+                    mz=massValues[result[[4]] + 1])
     result
 }
 
