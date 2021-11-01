acinostroza/TargetSearch

commit 23638f68c41d8f96978e89baf8650c67a5ea5b8a

Fix: plotRIdev use near equality for comparison
The function compares the selective or top masses agains the columns
of the intensity matrix. However, their lengths are variable, so the
for `if` condition throwed a warning if the lengths didn't match.

The fix uses the R function `all.equal` to test for near equality
instead, which is more robust for unequal lengths.

R/plot.R
@@ -2,7 +2,7 @@
 # function to make a boxplot of the RI deviations of a given metabolite.
 
 plotRIdev <- function(Lib, peaks, libId = 1) {
-
+	equal <- function(x, y) isTRUE(all.equal(x, y))
 	n <- length(libId)
 	j <- ceiling(sqrt(n))
 	i <- ceiling(n/j)
@@ -16,12 +16,13 @@ plotRIdev <- function(Lib, peaks, libId = 1) {
 		    stop(sprintf("Error: libId=%d not found", id))
 
 		x <- t(retIndex(peaks)[[id]])
+		mz <- as.numeric(colnames(x))
 		lib.name <- libName(Lib)[id]
-	    mz <- selMass(Lib)[[id]]
-    	if(any(mz != colnames(x)))
-        	mz <- topMass(Lib)[[id]]
-	    if(any(mz != colnames(x)))
-    	    stop("LibraryID Error: Library object and tsMSdata object don't match.")
+		sel_mz <- selMass(Lib)[[id]]
+		top_mz <- topMass(Lib)[[id]]
+
+		if(!equal(mz, top_mz) && !equal(mz, top_mz))
+			stop("LibraryID Error: Library object and tsMSdata object don't match.")
 
 		if(all(is.na(x))) {
 		    warning(sprintf("All values are NAs for libId=%d", id))

commit 2a98dc5c6eb0beb0091290b7b030cc673fafeea9

Fix: commit 23638f6 introduced a typo
Unfortunate typo. The check should be for both `sel_mz` and `top_mz`.

R/plot.R
@@ -21,7 +21,7 @@ plotRIdev <- function(Lib, peaks, libId = 1) {
 		sel_mz <- selMass(Lib)[[id]]
 		top_mz <- topMass(Lib)[[id]]
 
-		if(!equal(mz, top_mz) && !equal(mz, top_mz))
+		if(!equal(mz, sel_mz) && !equal(mz, top_mz))
 			stop("LibraryID Error: Library object and tsMSdata object don't match.")
 
 		if(all(is.na(x))) {