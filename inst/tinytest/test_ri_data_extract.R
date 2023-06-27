9a17c7c3d5f2689633b6eec1d48e8e6981bf4c22
Alvaro Cuadros-Inostroza <acuadros+github@gmail.com>
2023-06-27 22:32:58 +0200

    Tests: update ri_data_extract tests
    The tests use the internal ri_data_extract_text function to compare
    results with the C implementation. The returned values are identical
    or equivalent (due to rounding).

--- inst/tinytest/test_ri_data_extract.R
+++ inst/tinytest/test_ri_data_extract.R
@@ -1,29 +1,33 @@
 require('TargetSearchData')
 
-path <- file.path(find.package("TargetSearchData"), "gc-ms-data")
-id <- c(4,6,7,8,9,11,12,15)
+files <- tsd_rifiles()
 
-files <- file.path(path, sprintf('RI_7235eg%02d.txt', id))
+expect_true(all(file.exists(files)))
 
-expect_equal(file.exists(files), rep(TRUE, 8))
+# pure R implementation that works exactly as ri_data_extract
+ri_data_extract_text <- TargetSearch:::ri_data_extract_text
 
 a <- lapply(files, ri_data_extract,  204, c(200, 300), useRT=TRUE)
-b <- c(26L, 18L, 22L, 28L, 23L, 33L, 27L, 20L)
-expect_equal(sapply(a, nrow), b)
+b <- lapply(files, ri_data_extract_text,  204, c(200, 300), useRT=TRUE)
+expect_identical(a, b)
 
 a <- lapply(files, ri_data_extract, 116, c(200, 220) * 1000)
-b <- c(1L, 1L, 0L, 1L, 1L, 1L, 1L, 1L)
-expect_equal(sapply(a, nrow), b)
+b <- lapply(files, ri_data_extract_text, 116, c(200, 220) * 1000)
+expect_identical(a, b)
 
 a <- lapply(files, ri_data_extract, 1234, c(200, 220) * 1000)
-expect_equal(sapply(a, nrow), integer(8))
+expect_equal(sapply(a, nrow), integer(length(files)))
 
 # test mock data
 source('mock_functions.R')
 data <- mock_peak_data(mz_range=c(100,150), time_range=c(300, 400), n_peaks=200, n_scans=250, ri_factor=1004)
 myfile <- mock_write_file(data)
-m <- which(data$Peaks > 0)
-ret <- ri_data_extract(myfile, c(100:150), c(299, 401), useRT=TRUE)
-expect_equal( ret[, 'Intensity'] ,  data$Peaks[ m ] )
-ret <- ri_data_extract(myfile, c(100:150), c(299, 401) * 1004)
-expect_equal( ret[, 'Intensity'] ,  data$Peaks[ m ] )
+txtfile <- bin2text(myfile, paste0(myfile, ".txt"))
+
+a <- ri_data_extract(myfile, c(100:150), c(299, 401), useRT=TRUE)
+b <- ri_data_extract_text(txtfile, c(100:150), c(299, 401), useRT=TRUE)
+expect_equivalent(a, b)
+
+a <- ri_data_extract(myfile, c(100:150), c(299, 401) * 1004)
+b <- ri_data_extract_text(txtfile, c(100:150), c(299, 401) * 1004)
+expect_equivalent(a, b)
