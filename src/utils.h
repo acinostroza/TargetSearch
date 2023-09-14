5d1bf1a035f121736df29fb2164ab220834f25d7
Alvaro Cuadros-Inostroza <acuadros+github@gmail.com>
2023-09-14 12:59:13 +0200

    Refactor: remove useless ifndef
    The `ifndef` check was formally used for a standalone use of the
    functions contained in utils.c. This is not needed anymore because
    these functions depend on R function (findInterval, rPsort), so the
    code cannot be compiled without R.

--- src/utils.h
+++ src/utils.h
@@ -3,16 +3,6 @@
 
 #include <R.h>
 
-#ifndef USING_R
-	#include <stdlib.h>
-	#include <string.h>
-	#include <math.h>
-	#define Calloc(n,t) (t *) calloc( n, sizeof(t) )
-	#define Free(x) free(x)
-	#define Memzero(p,n) memset(p, 0, (n) * sizeof(*p))
-	#pragma message "not using R"
-#endif
-
 void moving(const int *, int, int, double *);
 void convolve(const int *, int, const double *, int, double *);
 int find_peak_diff(const double *, int, int *);
